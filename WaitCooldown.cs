b6485d8da96f1621186d43fe8bef61b7236f4303
Lime <demn2001@gmail.com>
2024-11-06 18:00:06 +0300

    add `WaitCooldown`

---
+++ WaitCooldown.cs
@@ -0,0 +1,24 @@
+﻿namespace Remote.Api;
+
+public class WaitCooldown(TimeSpan cooldown)
+{
+    private readonly SemaphoreSlim _lock = new SemaphoreSlim(1);
+
+    private DateTime nextTime = DateTime.Now;
+
+    public async Task Wait(CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _lock.WaitAsync(cancellationToken);
+            TimeSpan delta = nextTime - DateTime.Now;
+            if (delta.TotalSeconds > 0)
+                await Task.Delay(delta, cancellationToken);
+            nextTime = DateTime.Now.Add(cooldown);
+        }
+        finally
+        {
+            _lock.Release();
+        }
+    }
+}
